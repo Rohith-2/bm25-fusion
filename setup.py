8bd19a7fab2ecb9b045a838a038edc4a0891be22
Rohith Ramakrishnan <55501708+Rohith-2@users.noreply.github.com>
2025-02-15 11:23:50 +0530

    Changed PY version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     ],
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    python_requires=">=3.8",
+    python_requires=">=3.10",
     install_requires=[
         "numpy",
         "scipy",
